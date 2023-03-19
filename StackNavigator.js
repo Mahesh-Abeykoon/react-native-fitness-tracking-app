ca3dbc2adeedef236400a67e4ae6535e93b770f8
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-03-19 22:38:55 +0530

    Add Home screen to navigator

--- StackNavigator.js
+++ StackNavigator.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./HomeScreen";
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -9,7 +10,11 @@ const StackNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-    
+      <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
