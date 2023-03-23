cd71c8040a38a83706ffb1c10c2a79e6f2be5f71
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-03-23 23:38:09 +0530

    Add fitness cards

---
+++ components/fitness.js
@@ -0,0 +1,14 @@
+export default [
+    {
+      id: "0",
+      image:
+        "https://imgs.search.brave.com/2qLB62o2l7FDux9PylsvoJ9kCCdsLvIZW6FTSOUpD3I/rs:fit:844:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5m/YVRjZWk4MjE3TVFU/OEZYbl9TWElRSGFF/SyZwaWQ9QXBp",
+      name: "ARMS",
+    },
+    {
+      id: "1",
+      image:
+        "https://wallpaperforu.com/wp-content/uploads/2021/07/Wallpaper-Muscle-Press-Pose-Athlete-Workout-Gym-Fitn27-1536x1059.jpg",
+      name: "LEGS",
+    },
+  ];
