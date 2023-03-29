fd121648c2c3799dd31c8debef48a8c4c6dbdd66
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-03-29 23:50:10 +0530

    Add screen to navigator

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./HomeScreen";
 import WorkoutScreen from "./components/WorkoutScreen";
+import FitScreen from "./components/FitScreen";
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -13,7 +14,8 @@ const StackNavigator = () => {
       <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
       <Stack.Screen name="Workout" component={WorkoutScreen} options={{ headerShown: false }}/>
-       
+      <Stack.Screen name="Fit" component={FitScreen} options={{ headerShown: false }}/>
+
       </Stack.Navigator>
     </NavigationContainer>
   );
